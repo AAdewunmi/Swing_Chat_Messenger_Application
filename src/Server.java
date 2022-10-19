44993224b1e2ba13caa8a56cb2edb30b3819698c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-19 16:53:59 +0100

    Ninth Commit: Implement run() Client_Handler Sub-Class.

--- src/Server.java
+++ src/Server.java
@@ -32,43 +32,60 @@ public class Server {
             ++i;
 
         }
-
     }
-
-    static class Client_Handler implements Runnable{
-
-        Socket connection;
-        PrintWriter output;
-        BufferedReader input;
-        String clientName;
-
-        public Client_Handler(String clientName, Socket connection, PrintWriter output, BufferedReader input) {
-            this.connection = connection;
-            this.output = output;
-            this.input = input;
-            this.clientName = clientName;
-        }
-
-        String receiveMessage() throws IOException{
-            return input.readLine();
-        }
-
-        void sendMessage(String message){
-            output.println(message);
-        }
-
-        void closeStreams() throws IOException{
-            output.close();
-            input.close();
-            connection.close();
-            Server.sockets.remove(this);
-            System.out.println("Current Client Count - " + Server.sockets.size());
-        }
-
-        @Override
-        public void run() {
-
-        }
-    }
-
 }
+
+class Client_Handler implements Runnable{
+
+    Socket connection;
+    PrintWriter output;
+    BufferedReader input;
+    String clientName;
+
+    public Client_Handler(String clientName, Socket connection, PrintWriter output, BufferedReader input) {
+        this.connection = connection;
+        this.output = output;
+        this.input = input;
+        this.clientName = clientName;
+    }
+
+    String receiveMessage() throws IOException{
+        return input.readLine();
+    }
+
+    void sendMessage(String message){
+        output.println(message);
+    }
+
+    void closeStreams() throws IOException{
+        output.close();
+        input.close();
+        connection.close();
+        Server.sockets.remove(this);
+        System.out.println("Current Client Count - " + Server.sockets.size());
+    }
+
+    @Override
+    public void run() {
+        try{
+            while(true){
+                String message = receiveMessage();
+                if (message.endsWith("Client_wants_to_end_the_connection")) {
+                    message = message.substring(0, message.indexOf('-')) + " has left the Server";
+                    for (Client_Handler clientHandler: Server.sockets){
+                        if (!clientHandler.clientName.equals(clientName)) {clientHandler.sendMessage(message);};
+                    }
+                    closeStreams();
+                    break;
+                }
+                for (Client_Handler clientHandler: Server.sockets) {
+                    if (!clientHandler.clientName.equals(clientName)) {clientHandler.sendMessage(message);}
+                }
+            }
+        }catch(Exception e){
+            System.out.println("Error: run()");
+            e.printStackTrace();
+        }
+    }
+}
+
