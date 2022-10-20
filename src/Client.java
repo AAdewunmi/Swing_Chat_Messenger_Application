f1b0d72a755b495ed44a49e0aca4efb299285af0
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-20 11:22:05 +0100

    Twelfth Commit: Implement Skeleton Client Class.

--- src/Client.java
+++ src/Client.java
@@ -2,7 +2,18 @@
 /**
  * Client Class
  * Client Socket running at port 2020
- * Ngrok Config: Server Address ->  // Port Number ->
+ * Ngrok Config: Server Address -> "X.tcp.ngrok.io" // Port Number ->
  */
 public class Client {
+    public static void main(String[] args) {
+
+        // Localhost Configuration
+        Client_SetUp clientSetUp = new Client_SetUp("localhost");
+        clientSetUp.startRunning();
+
+        // Ngrok Configuration
+        /*Client_SetUp clientSetUp = new Client_SetUp("X.tcp.ngrok.io");
+        clientSetUp.startRunning();*/
+
+    }
 }
