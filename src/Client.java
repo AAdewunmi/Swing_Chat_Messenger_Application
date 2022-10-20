3a621f59733401858c8af6659c5d16ebefa8a051
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-20 13:48:33 +0100

    Fifteen Commit: Create Constructor for Client_SetUp Sub-Class.

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -34,5 +37,67 @@ class Client_SetUp extends JFrame{
     private String name;
     private JFrame f1, f2;
 
+    public Client_SetUp(String host){
 
-}
+        f1 = new JFrame();
+        serverIP = host;
+        f1.setSize(300, 500);
+        JLabel jLabel = new JLabel("Enter Your Name: ");
+        JTextField jTextField = new JTextField(20);
+        JButton jButton = new JButton("Submit");
+        JPanel jPanel = new JPanel();
+        jPanel.add(jLabel);
+        jPanel.add(jTextField);
+        jPanel.add(jButton);
+        f1.add(jPanel);
+
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                name = jTextField.getText();
+                f1.setVisible(false);
+                f1.dispose();
+                f2.setVisible(true);
+            }
+        });
+
+        f1.setVisible(true);
+        f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        f2 = new JFrame();
+        chatWindow = new JTextArea();
+        userText = new JTextField();
+        Button buttonEnd = new Button("End");
+        userText.setEditable(true);
+        chatWindow.setEditable(false);
+        userText.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendMessage(e.getActionCommand());
+                userText.setText("");
+            }
+        });
+        buttonEnd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendMessage("Client_wants_to_end_the_connection");
+                CloseStreams();
+                f2.dispose();
+                System.exit(0);
+            }
+        });
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        mainPanel.add(bottomPanel, BorderLayout.PAGE_END);
+        mainPanel.add(new JScrollPane(chatWindow), BorderLayout.CENTER);
+        bottomPanel.add(userText, BorderLayout.CENTER);
+        bottomPanel.add(buttonEnd, BorderLayout.LINE_END);
+        f2.getContentPane().add(mainPanel);
+        f2.setSize(300, 500);
+        f2.setVisible(false);
+        f2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+    } // End Constructor
+
+} // End Client_SetUp
